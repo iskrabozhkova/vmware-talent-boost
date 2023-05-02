0c146ffe4cca5c5fd261d7658efb20686f04e57f
iskrabozhkova <52824905+iskrabozhkova@users.noreply.github.com>
2023-05-02 15:07:38 +0300

    version-changed

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -12,8 +12,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-	@GetMapping("/")
-	public String hello(){
-		return "Hello world";
-	}
+//	@GetMapping("/")
+//	public String hello(){
+//		return "Hello world";
+//	}
 }
